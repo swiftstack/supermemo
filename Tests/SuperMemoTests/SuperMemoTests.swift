76229258ae3e03eeee91cfe07f917e25c3e244e5
Tony Freeman <tonyfreeman@users.noreply.github.com>
2020-05-09 01:30:53 +0300

    [Tests] Use new api

--- Tests/SuperMemoTests/SuperMemoTests.swift
+++ Tests/SuperMemoTests/SuperMemoTests.swift
@@ -4,60 +4,60 @@ import Test
 final class SuperMemoTests: TestCase {
     func testEasinessFactor() {
         let factor = SuperMemo2.EasinessFactor(1.5)
-        assertEqual(factor.value, 1.5)
+        expect(factor.value == 1.5)
 
         let min = SuperMemo2.EasinessFactor(1.2)
-        assertEqual(min.value, 1.3)
+        expect(min.value == 1.3)
 
         let max = SuperMemo2.EasinessFactor(2.6)
-        assertEqual(max.value, 2.5)
+        expect(max.value == 2.5)
     }
 
     func testRepetition() {
         var repetition = SuperMemo2.Repetition.initial
-        assertEqual(repetition.number, 0)
-        assertEqual(repetition.interval, 0)
+        expect(repetition.number == 0)
+        expect(repetition.interval == 0)
 
         repetition.next(forFactor: 0)
 
-        assertEqual(repetition.number, 1)
-        assertEqual(repetition.interval, 1)
+        expect(repetition.number == 1)
+        expect(repetition.interval == 1)
 
         repetition.next(forFactor: 0)
 
-        assertEqual(repetition.number, 2)
-        assertEqual(repetition.interval, 6)
+        expect(repetition.number == 2)
+        expect(repetition.interval == 6)
 
         repetition.next(forFactor: 0)
 
-        assertEqual(repetition.number, 3)
-        assertEqual(repetition.interval, 8)
+        expect(repetition.number == 3)
+        expect(repetition.interval == 8)
     }
 
     func testItem() {
         var item = SuperMemo2.Item()
         var repetition = SuperMemo2.Repetition.initial
-        assertEqual(item.factor, 2.5)
-        assertEqual(item.repetition, repetition)
+        expect(item.factor == 2.5)
+        expect(item.repetition == repetition)
 
         item.apply(response: .difficult)
         repetition.next(forFactor: item.factor)
-        assertEqual(item.factor, 1.3)
-        assertEqual(item.repetition, repetition)
+        expect(item.factor == 1.3)
+        expect(item.repetition == repetition)
 
         item.apply(response: .perfect)
         repetition.next(forFactor: item.factor)
-        assertEqual(Int(item.factor.value * 10), 14)
-        assertEqual(item.repetition, repetition)
+        expect(Int(item.factor.value * 10) == 14)
+        expect(item.repetition == repetition)
 
         item.apply(response: .perfect)
         repetition.next(forFactor: item.factor)
-        assertEqual(Int(item.factor.value * 10), 15)
-        assertEqual(item.repetition, repetition)
+        expect(Int(item.factor.value * 10) == 15)
+        expect(item.repetition == repetition)
 
         item.apply(response: .blackout)
         repetition = .initial
-        assertEqual(Int(item.factor.value * 10), 15)
-        assertEqual(item.repetition, repetition)
+        expect(Int(item.factor.value * 10) == 15)
+        expect(item.repetition == repetition)
     }
 }
